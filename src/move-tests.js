96c9e22fb2e97d263ef79ab218aafaac7f128e69
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-02-21 13:16:45 +0100

    PAWN ok / ROOK fixme

--- src/move-tests.js
+++ src/move-tests.js
@@ -1,4 +1,12 @@
 import { PieceEnum, getX, getY, getId } from './util';
+import Position from './Position';
+
+function move(squares, from, to) {
+  const buffer = squares.slice();
+  buffer[to] = squares[from];
+  buffer[from] = null;
+  return buffer;
+}
 
 function pieceMoves(square, squares){
   if (squares[square]){
@@ -22,30 +30,33 @@ function pieceMoves(square, squares){
         return queenMoves(square, squares);
 
       default:
-        return [];
+        return null;
     }
   }
 }
-
-function pieceEats(){}
 
 //////////////// PAWN ////////////////
 
 // TODO : check check (sans mauvais jeu de mot)
 function pawnMoves(square, squares) {
+    return { moves: pawnDeplacements(square, squares), eats: pawnEats(square, squares) }
+}
+
+function pawnDeplacements(square, squares) {
   const player = squares[square].player;
   const moves = [];
   const x = getX(square);
   const y = getY(square);
 
   if (player === "white") {
+
     let testOne = getId(x, y - 1);
     if(testOne >= 0 && !squares[testOne]) {
-      moves = [ ...moves, testOne];
+      moves.push(testOne);
       if(y === 6) {
         let testTwo = getId(x, 4);
         if (!squares[testTwo]) {
-          moves = [ ...moves, testTwo];
+          moves.push(testTwo);
         }
       }
     }
@@ -53,11 +64,11 @@ function pawnMoves(square, squares) {
   else {
     let testOne = getId(x, y + 1);
     if(testOne < 64 && !squares[testOne]) {
-      moves = [ ...moves, testOne];
+      moves.push(testOne);
       if(y === 1) {
         let testTwo = getId(x, 3);
         if (!squares[testTwo]) {
-          moves = [ ...moves, testTwo];
+          moves.push(testTwo);
         }
       }
     }
@@ -66,21 +77,83 @@ function pawnMoves(square, squares) {
   return moves;
 }
 
-function pawnEats(square, player) {
+function pawnEats(square, squares) {
+  const player = squares[square].player;
+  const eats = [];
+  const pos = new Position(square);
+
+  if (player === "white") {
+    // TODO
+  }
+  else {
+    // TODO
+  }
+
   return [];
 }
 
 //////////////// ROOK ////////////////
 
-function rookMoves() {}
+function rookMoves(square, squares) {
+  const moves = [];
+  const eats = [];
+  const player = squares[square].player;
+  const pos = new Position(square);
 
-// function rookEats() {}
+  let test = 0;
+
+  while (pos.addX(1) && test < 12){
+    test++;
+  }
+  //
+  // // vers le bas
+  // while (pos.addX(1) && !square[pos.i]) {
+  //   moves.push(pos.i);
+  // }
+  // pos.addX(1);
+  // if (square[pos.i] && square[pos.i].player !== player) {
+  //   eats.push(pos.i);
+  // }
+  //
+  //
+  // // vers le haut
+  // pos.setI(square);
+  // while (pos.addX(-1) && !square[pos.i]) {
+  //   moves.push(pos.i);
+  // }
+  // pos.addX(-1);
+  // if (square[pos.i] && square[pos.i].player !== player) {
+  //   eats.push(pos.i);
+  // }
+  //
+  // // droite
+  // pos.setI(square);
+  // while (pos.addY(1) && !square[pos.i]) {
+  //   moves.push(pos.i);
+  // }
+  // pos.addY(1);
+  // if (square[pos.i] && square[pos.i].player !== player) {
+  //   eats.push(pos.i);
+  // }
+  //
+  // // gauche
+  // pos.setI(square);
+  // while (pos.addY(-1) && !square[pos.i]) {
+  //   moves.push  (pos.i);
+  // }
+  // pos.addY(-1);
+  // if (square[pos.i] && square[pos.i].player !== player) {
+  //   eats.push(pos.i);
+  // }
+
+  return { moves: moves, eats: eats };
+
+}
 
 //////////////// KNIGHT ////////////////
 
 function knightMoves() {}
 
-// eats
 
 //////////////// BISHOP ////////////////
 
@@ -100,4 +173,6 @@ function queenMoves() {}
 
 // eats
 
-export { pieceMoves, pieceEats };
+
+
+export { pieceMoves, move };
