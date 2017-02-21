c6eda00c69035afe0b9d6ca2d74dcae8b660c208
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-02-21 13:14:58 +0100

    Util : renommage + ajout
    - renomme les valeurs de l'enum pieces en string au lieu de number
    - déplacement de getDefaultPieces vers Util

--- src/Game.js
+++ src/Game.js
@@ -1,39 +1,8 @@
 import React, { Component } from 'react';
 import Board from './Board';
-import { PieceEnum } from './util';
+import { PieceEnum, getDefaultPieces } from './util';
 import { pieceMoves } from './move-tests'
 // import { getX, getY, getId, PieceEnum } from './util';
-
-function getDefaultPieces() {
-
-  const squares = Array(64).fill(null);
-
-  squares[56] = { piece: PieceEnum.ROOK, player: "white" };
-  squares[63] = { piece: PieceEnum.ROOK, player: "white" };
-  squares[57] = { piece: PieceEnum.KNIGHT, player: "white" };
-  squares[62] = { piece: PieceEnum.KNIGHT, player: "white" };
-  squares[58] = { piece: PieceEnum.BISHOP, player: "white" };
-  squares[61] = { piece: PieceEnum.BISHOP, player: "white" };
-  squares[59] = { piece: PieceEnum.QUEEN, player: "white" };
-  squares[60] = { piece: PieceEnum.KING, player: "white" };
-  for (let i = 48; i<56; i++){
-    squares[i] = { piece: PieceEnum.PAWN, player: "white" };
-  }
-
-  squares[0] = { piece: PieceEnum.ROOK, player: "black" };
-  squares[7] = { piece: PieceEnum.ROOK, player: "black" };
-  squares[1] = { piece: PieceEnum.KNIGHT, player: "black" };
-  squares[6] = { piece: PieceEnum.KNIGHT, player: "black" };
-  squares[2] = { piece: PieceEnum.BISHOP, player: "black" };
-  squares[5] = { piece: PieceEnum.BISHOP, player: "black" };
-  squares[3] = { piece: PieceEnum.QUEEN, player: "black" };
-  squares[4] = { piece: PieceEnum.KING, player: "black" };
-  for (let i = 8; i<16; i++){
-    squares[i] = { piece: PieceEnum.PAWN, player: "black" };
-  }
-
-  return squares;
-}
 
 class Game extends Component{
   constructor() {
--- src/Util.js
+++ src/Util.js
@@ -1,20 +1,55 @@
 const PieceEnum = Object.freeze({
-  KING: 1,
-  QUEEN: 2,
-  ROOK: 3,
-  BISHOP: 4,
-  KNIGHT: 5,
-  PAWN: 6,
+  KING: "king",
+  QUEEN: "queen",
+  ROOK: "rook",
+  BISHOP: "bishop",
+  KNIGHT: "knight",
+  PAWN: "pawn",
 });
+
+function getDefaultPieces() {
+
+  const squares = Array(64).fill(null);
+  const w = "white";
+  const b = "black";
+
+  squares[56] = { piece: PieceEnum.ROOK, player: w };
+  squares[63] = { piece: PieceEnum.ROOK, player: w };
+  squares[57] = { piece: PieceEnum.KNIGHT, player: w };
+  squares[62] = { piece: PieceEnum.KNIGHT, player: w };
+  squares[58] = { piece: PieceEnum.BISHOP, player: w };
+  squares[61] = { piece: PieceEnum.BISHOP, player: w };
+  squares[59] = { piece: PieceEnum.QUEEN, player: w };
+  squares[60] = { piece: PieceEnum.KING, player: w };
+  for (let i = 48; i < 56; i++){
+    squares[i] = { piece: PieceEnum.PAWN, player: w };
+  }
+
+  squares[0] = { piece: PieceEnum.ROOK, player: b };
+  squares[7] = { piece: PieceEnum.ROOK, player: b };
+  squares[1] = { piece: PieceEnum.KNIGHT, player: b };
+  squares[6] = { piece: PieceEnum.KNIGHT, player: b };
+  squares[2] = { piece: PieceEnum.BISHOP, player: b };
+  squares[5] = { piece: PieceEnum.BISHOP, player: b };
+  squares[3] = { piece: PieceEnum.QUEEN, player: b };
+  squares[4] = { piece: PieceEnum.KING, player: b };
+  for (let i = 8; i < 16; i++){
+    squares[i] = { piece: PieceEnum.PAWN, player: b };
+  }
+
+  return squares;
+}
 
 function getX(i) {
   return i%8;
 }
+
 function getY(i) {
   return parseInt(i/8, 10);
 }
+
 function getId(x, y) {
   return y*8+x;
 }
 
-export { getX, getY, getId, PieceEnum };
+export {  PieceEnum, getX, getY, getId, getDefaultPieces };
