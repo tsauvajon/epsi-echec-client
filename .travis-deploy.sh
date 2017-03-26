df9a2418242ae7213a5a2fbf3c208f71f394def8
tsauvajon <thomas.sauvajon.dev@gmail.com>
2017-03-26 17:03:08 +0200

    ...

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -49,8 +49,11 @@ ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 #cp deploy_key ~/.ssh/id_rsa
-DISPLAY= SSH_ASKPASS=/bin/true ssh-add
+PS=""
+install -vm700 <(echo "echo $PS") $PWD/my_pass
 eval `ssh-agent -s`
+DISPLAY= SSH_ASKPASS=$PWD/my_pass ssh-add - && rm -v my_pass
+# cp deploy_key ~/.ssh/id_rsa
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
