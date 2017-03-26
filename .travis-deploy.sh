d3eaa3ecdfe19b3baa9c74ecb58c04240e0cea54
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2017-03-26 13:50:38 +0200

    fixed adding ssh key
    cp it instead of eval && ssh-add in order for Travis not to ask for a password

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -44,8 +44,9 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+cp deploy_key ~/.ssh/id_rsa
+# eval `ssh-agent -s`
+# ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 cd build
